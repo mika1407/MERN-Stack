5d4d186c5dae08fe57e089967e89f923bd3c6ce1
mika tiihonen <koneautomaatio@hotmail.com>
2022-03-14 21:11:15 +0200

    Created Goal Extra Reducer

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
+import GoalForm from '../components/GoalForm'
+
+import { FaYoutube } from "react-icons/fa";
 
 
 function Dashboard() {
@@ -14,7 +17,35 @@ function Dashboard() {
   }, [user, navigate])
 
   return (
-    <div>Dashboard</div>
+    <>
+      <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+
+      <GoalForm />
+
+      {/* <section className='content'>
+        {goals.length > 0 ? (
+          <div className='goals'>
+            {goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (
+          <h3>You have not set any goals</h3>
+        )}
+      </section> */}
+
+    <section className='omabottom'>
+      <p>Youtube videot miten nämä sivut tehtiin:</p>
+      <h2>
+        <a href="https://www.youtube.com/watch?v=UXjMo25Nnvc&t" target="_blank" rel="noreferrer noopener">
+          <FaYoutube />  part 4
+        </a>
+      </h2>
+    </section>
+    </>
   )
 }
 
